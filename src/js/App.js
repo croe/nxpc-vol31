1586bbc43636f6524ced6b9f374695fd853a7a11
Ayumu Nagamatsu <ayumu.nagamatsu@gmail.com>
2018-03-13 04:20:43 +0900

    update style.

--- src/js/App.js
+++ src/js/App.js
@@ -1,14 +1,12 @@
 import $ from 'jquery';
 import setUA from './util/setUaBodyClass';
 import Util from './Util.js';
-import GLScene from './gl/scene'
 
 import Map from './_map';
 
 // Application entry point
 $(()=>{
   setUA();
-  new GLScene();
   Map();
 
   $("a[href^='#']").on('click', function () {
